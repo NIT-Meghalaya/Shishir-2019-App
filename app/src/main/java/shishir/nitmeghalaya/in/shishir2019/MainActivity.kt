a4ef543db797b68d9c1c7a0b7db03367083041e1
Devansh Maurya <Devansh233@gmail.com>
2019-01-15 00:15:42 +0530

    Added android core kotlin plugins library

--- app/src/main/java/shishir/nitmeghalaya/in/shishir2019/MainActivity.kt
+++ app/src/main/java/shishir/nitmeghalaya/in/shishir2019/MainActivity.kt
@@ -1,6 +1,6 @@
 package shishir.nitmeghalaya.`in`.shishir2019
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
