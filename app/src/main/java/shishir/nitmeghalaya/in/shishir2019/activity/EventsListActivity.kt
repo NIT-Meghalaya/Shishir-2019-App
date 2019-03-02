fc54147d7f9a80e276854ea9ef4e4cd8e9a41a7e
Devansh Maurya <Devansh233@gmail.com>
2019-03-02 22:18:26 +0530

    Using Glide to load images in list. Also added method to add gradient dynamically. Glide image loading is not working without a scroll

--- app/src/main/java/shishir/nitmeghalaya/in/shishir2019/activity/EventsListActivity.kt
+++ app/src/main/java/shishir/nitmeghalaya/in/shishir2019/activity/EventsListActivity.kt
@@ -6,7 +6,6 @@ import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_events_list.*
-import kotlinx.android.synthetic.main.event_item.*
 import shishir.nitmeghalaya.`in`.shishir2019.R
 import shishir.nitmeghalaya.`in`.shishir2019.adapter.EventsListAdapter
 import shishir.nitmeghalaya.`in`.shishir2019.models.EventCard
@@ -18,13 +17,12 @@ class EventsListActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events_list)
 
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
-        eventListItemImage
         val items = ArrayList<EventCard>()
         for (i in 0..10)
             items.add(EventCard("Type $i", "Title $i"))
 
         val adapter = EventsListAdapter(items,applicationContext)
-        recyclerView.adapter = adapter
+        eventsRecyclerView.adapter = adapter
+        eventsRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
     }
 }
--- app/src/main/java/shishir/nitmeghalaya/in/shishir2019/viewholder/EventsListItemViewHolder.kt
+++ app/src/main/java/shishir/nitmeghalaya/in/shishir2019/viewholder/EventsListItemViewHolder.kt
@@ -1,13 +1,19 @@
 package shishir.nitmeghalaya.`in`.shishir2019.viewholder
 
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.GradientDrawable
 import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.event_item.view.*
 import shishir.nitmeghalaya.`in`.shishir2019.R
 import shishir.nitmeghalaya.`in`.shishir2019.activity.EventDetailActivity
 import shishir.nitmeghalaya.`in`.shishir2019.models.EventCard
+import android.graphics.BitmapFactory
+import android.graphics.Bitmap
 
 /**
  * Created by Devansh on 24/1/19.
@@ -15,15 +21,35 @@ import shishir.nitmeghalaya.`in`.shishir2019.models.EventCard
 
 class EventsListItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
+    private val context = view.context
+
     fun bind(item: EventCard) {
         view.apply {
             eventListItemType.text = item.type
             eventListItemTitle.text = item.title
+            //eventListItemImage.setImageResource(R.drawable.samplebg)
             Glide.with(this).load(R.drawable.samplebg).into(eventListItemImage)
             setOnClickListener {
                 val intent = Intent(view.context, EventDetailActivity::class.java)
                 view.context.startActivity(intent)
             }
+
+//            if (android.os.Build.VERSION.SDK_INT >= 23)
+//                foreground = createForegroundGradient()
         }
+    }
+
+    private fun createForegroundGradient(): GradientDrawable {
+        val eventImageBitmap = BitmapFactory.decodeResource(context.resources, R.drawable.samplebg)
+
+        val palette = Palette.from(eventImageBitmap).generate()
+
+        val gradientColorsArray: IntArray = intArrayOf(
+            palette.getMutedColor(ContextCompat.getColor(context, R.color.black)),
+            palette.getVibrantColor(ContextCompat.getColor(context, R.color.transparent)),
+            ContextCompat.getColor(context, R.color.transparent)
+        )
+
+        return GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, gradientColorsArray)
     }
 }
