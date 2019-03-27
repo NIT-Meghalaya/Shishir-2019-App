6bf8a39aa01633ce88720ac2c41108a195ef48da
Devansh Maurya <Devansh233@gmail.com>
2019-03-27 09:40:52 +0530

    Now showing team members info

--- app/src/main/java/shishir/nitmeghalaya/in/shishir2019/fragment/TeamFragment.kt
+++ app/src/main/java/shishir/nitmeghalaya/in/shishir2019/fragment/TeamFragment.kt
@@ -2,7 +2,6 @@ package shishir.nitmeghalaya.`in`.shishir2019.fragment
 
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,14 +9,15 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.gson.Gson
 
 import shishir.nitmeghalaya.`in`.shishir2019.R
 import shishir.nitmeghalaya.`in`.shishir2019.adapter.TeamMembersRecyclerViewAdapter
 import shishir.nitmeghalaya.`in`.shishir2019.models.TeamMember
+import shishir.nitmeghalaya.`in`.shishir2019.util.getJsonFromList
+import shishir.nitmeghalaya.`in`.shishir2019.util.getListFromJson
 
 class TeamFragment : Fragment() {
-
-    private var teamMembersList = arrayListOf<TeamMember>()
 
     companion object {
         @JvmStatic
@@ -30,20 +30,26 @@ class TeamFragment : Fragment() {
         val recyclerView = inflater.inflate(
                 R.layout.fragment_team, container, false) as RecyclerView
         getTeamMembersFromDatabase(recyclerView)
-        return view
+        return recyclerView
     }
 
     private fun getTeamMembersFromDatabase(recyclerView: RecyclerView) {
         val db = FirebaseFirestore.getInstance()
         db.collection("coreTeams").document("teamMembers").get()
                 .addOnSuccessListener {documentSnapshot ->
+                    var teamMembersList: ArrayList<TeamMember> = arrayListOf()
                     documentSnapshot.data?.forEach {
-                        teamMembersList = it.value as ArrayList<TeamMember>
+                        teamMembersList = makeTeamMembersList(it.value as ArrayList<TeamMember>)
                     }
                     recyclerView.apply {
                         adapter = TeamMembersRecyclerViewAdapter(context, teamMembersList)
                         layoutManager = LinearLayoutManager(context)
                     }
                 }
+    }
+
+    private fun makeTeamMembersList(teamMembersListMap: ArrayList<TeamMember>): ArrayList<TeamMember> {
+        val json = Gson().getJsonFromList<ArrayList<TeamMember>>(teamMembersListMap)
+        return Gson().getListFromJson(json)
     }
 }
--- app/src/main/java/shishir/nitmeghalaya/in/shishir2019/models/TeamMember.kt
+++ app/src/main/java/shishir/nitmeghalaya/in/shishir2019/models/TeamMember.kt
@@ -8,7 +8,7 @@ data class TeamMember(
         val post: String = "",
         val email: String = "",
         val contact: String = "",
-        val event: String = "",
         val image: String = "",
+        val team: String = "",
         var imageResId: Int = 0
 )
