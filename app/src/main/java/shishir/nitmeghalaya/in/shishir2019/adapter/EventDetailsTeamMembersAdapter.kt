3a8a58a4354ce4513cc21a72da2df5ec287719fb
Devansh Maurya <Devansh233@gmail.com>
2019-03-27 12:04:09 +0530

    Changed team member layout in event details

--- app/src/main/java/shishir/nitmeghalaya/in/shishir2019/adapter/EventDetailsTeamMembersAdapter.kt
+++ app/src/main/java/shishir/nitmeghalaya/in/shishir2019/adapter/EventDetailsTeamMembersAdapter.kt
@@ -1,17 +1,27 @@
 package shishir.nitmeghalaya.`in`.shishir2019.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import shishir.nitmeghalaya.`in`.shishir2019.R
 import shishir.nitmeghalaya.`in`.shishir2019.models.TeamMember
+import shishir.nitmeghalaya.`in`.shishir2019.util.getImageResource
 import shishir.nitmeghalaya.`in`.shishir2019.viewholder.EventDetailTeamMemberViewHolder
 
 /**
  * Created by Devansh on 26/3/19.
  */
-class EventDetailsTeamMemebersAdapter(private val teamMembers: List<TeamMember>):
+class EventDetailsTeamMembersAdapter(private val context: Context,
+        private val teamMembers: List<TeamMember>):
         RecyclerView.Adapter<EventDetailTeamMemberViewHolder>() {
+
+    init {
+        teamMembers.forEach {
+            it.imageResId = if (it.image != "") getImageResource(context, it.image)
+                            else R.drawable.shishir_logo
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventDetailTeamMemberViewHolder {
         val view = LayoutInflater.from(parent.context)
