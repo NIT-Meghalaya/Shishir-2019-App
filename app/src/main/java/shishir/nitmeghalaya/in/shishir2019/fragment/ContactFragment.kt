2c9bee62d647e3040976291926d79fc3a5938f13
ichimichi <ichimichilaribok@gmail.com>
2019-03-28 16:25:41 +0530

    implememted click actions on buttons

--- app/src/main/java/shishir/nitmeghalaya/in/shishir2019/fragment/ContactFragment.kt
+++ app/src/main/java/shishir/nitmeghalaya/in/shishir2019/fragment/ContactFragment.kt
@@ -6,7 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_contact.*
 import shishir.nitmeghalaya.`in`.shishir2019.R
+import android.content.Intent
+import android.net.Uri
+import android.R.id.message
+
+
+
 
 class ContactFragment : Fragment() {
 
@@ -14,7 +21,43 @@ class ContactFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_contact, container, false)
         //TODO: Add actions for buttons
 
+
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnEmail.setOnClickListener({
+            val intent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "shishir@nitm.ac.in", null))
+            startActivity(Intent.createChooser(intent, "Choose an Email client :"))
+        })
+
+        btnWebsite.setOnClickListener({
+            val uri = Uri.parse("http://nitmeghalaya.in/shishir/")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        })
+
+        btnFacebook.setOnClickListener({
+            val uri = Uri.parse("https://www.facebook.com/shishir.nitm/")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        })
+
+//        btnInstagram.setOnClickListener({
+//            val uri = Uri.parse("http://www.nitmeghalaya.in/shishir/#")
+//            val intent = Intent(Intent.ACTION_VIEW, uri)
+//            startActivity(intent)
+//        })
+//
+//        btnYoutube.setOnClickListener({
+//            val uri = Uri.parse("http://www.nitmeghalaya.in/shishir/#")
+//            val intent = Intent(Intent.ACTION_VIEW, uri)
+//            startActivity(intent)
+//        })
+
+
     }
 
     companion object {
