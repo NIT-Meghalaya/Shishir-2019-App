80e70baa55de1c6f3fe287e8610e4c14a3b37aac
Devansh Maurya <Devansh233@gmail.com>
2019-03-29 16:26:11 +0530

    Added code to control text animation on long text in TeamMember fragment programatically

--- app/src/main/java/shishir/nitmeghalaya/in/shishir2019/viewholder/TeamMemberItemViewHolder.kt
+++ app/src/main/java/shishir/nitmeghalaya/in/shishir2019/viewholder/TeamMemberItemViewHolder.kt
@@ -4,6 +4,20 @@ import android.view.View
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_team_member.view.*
 import shishir.nitmeghalaya.`in`.shishir2019.models.TeamMember
+import android.opengl.ETC1.getWidth
+import android.R.attr.x
+import android.content.Context
+import android.os.Build
+import android.view.Display
+import android.content.Context.WINDOW_SERVICE
+import android.graphics.Point
+import androidx.core.content.ContextCompat.getSystemService
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.TranslateAnimation
+import android.widget.TextView
+
+
 
 /**
  * Created by Devansh on 26/3/19.
@@ -15,7 +29,7 @@ class TeamMemberItemViewHolder(view: View): BaseViewHolder(view) {
 
         view.apply {
             name.text = item.name
-            name.isSelected = true
+            animateTextView(name, view.context)
 
             if (item.team.isNotEmpty())
                 post.text = "${item.post} (${item.team})"
@@ -23,10 +37,47 @@ class TeamMemberItemViewHolder(view: View): BaseViewHolder(view) {
                 post.text = item.post
 
             email.text = item.email
-            email.isSelected = true
+            animateTextView(email, view.context)
 
             contact.text = item.contact
             Glide.with(view).load(item.imageResId).into(memberImageView)
         }
+    }
+
+    private fun animateTextView(mTextView: TextView, context: Context) {
+        val textWidth = getTextViewWidth(mTextView)
+        val displayWidth = getDisplayWidth(context)
+
+        /* Start animation only when text is longer than dislay width. */
+        if (displayWidth <= textWidth) {
+            val mAnimation = TranslateAnimation(
+                    0f, (-textWidth).toFloat(),
+                    0f, 0f)
+            mAnimation.duration = 10000    // Set custom duration.
+            mAnimation.startOffset = 1000    // Set custom offset.
+            mAnimation.repeatMode = Animation.RESTART    // This will animate text back ater it reaches end.
+            mAnimation.repeatCount = Animation.INFINITE    // Infinite animation.
+
+            mTextView.startAnimation(mAnimation)
+        }
+    }
+
+    private fun getDisplayWidth(context: Context): Int {
+        val displayWidth: Int
+
+        val windowManager = context.getSystemService(
+                Context.WINDOW_SERVICE) as WindowManager
+        val display = windowManager.defaultDisplay
+        val screenSize = Point()
+
+        display.getSize(screenSize)
+        displayWidth = screenSize.x
+
+        return displayWidth
+    }
+
+    private fun getTextViewWidth(textView: TextView): Int {
+        textView.measure(0, 0)    // Need to set measure to (0, 0).
+        return textView.measuredWidth
     }
 }
